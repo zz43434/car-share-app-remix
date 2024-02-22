443ed9e2a0403912cd21836db0a1e365df0138f5
zrahman <zahid.rahman@ajbell.co.uk>
2024-02-22 21:07:49 +0000

    Stripped back homepage, renamed env file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,9 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'media',
   theme: {
+    darkMode: 'media',
     extend: {},
   },
   plugins: [],
